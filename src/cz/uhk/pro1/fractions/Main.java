5cb3c45bfa741a98be292acb99e3065d9a5d8a1c
Pavel Kriz <pavel.kriz@uhk.cz>
2021-03-05 11:15:13 +0100

    Exercising

--- src/cz/uhk/pro1/fractions/Main.java
+++ src/cz/uhk/pro1/fractions/Main.java
@@ -1,19 +1,48 @@
 package cz.uhk.pro1.fractions;
 
 import cz.uhk.pro1.fractions.model.Fraction;
+import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Fraction fraction = new Fraction(1,0);
+        Random rand = new Random();
 
-        double d = fraction.toDouble();
-        System.out.println(d);
+        String answer;
+        do {
 
-        //String s = fraction.toString();
-        System.out.println(fraction);
+            Fraction fraction = new Fraction(rand.nextInt(10),rand.nextInt(10) + 1);
+            Fraction fraction2 = new Fraction(rand.nextInt(10),rand.nextInt(10) + 1);
+            int choice = rand.nextInt(4);
+            Fraction correctResult;
+            String operator;
 
-        //fraction.setDenominator(0);
+            switch (choice) {
+                case 0:
+                    operator = " + ";
+                    correctResult = fraction.plus(fraction2);
+                    break;
+                case 1:
+                    operator = " - ";
+                    correctResult = fraction.minus(fraction2);
+                    break;
+                case 2:
+                    operator = " * ";
+                    correctResult = fraction.mult(fraction2);
+                    break;
+                default:
+                    operator = " / ";
+                    correctResult = fraction.div(fraction2);
+            }
 
+            answer = JOptionPane.showInputDialog("Vypočítej " + fraction + operator + fraction2 + " a zadej výsledek ve tvaru čitatel/jmenoval");
+            if(correctResult.toString().equals(answer)) {
+                System.out.println("Šikulka");
+            } else {
+                System.out.println("Nope, správně je: " + correctResult);
+            }
 
+        } while (!(answer == null || answer.equals("konec")));  // skoncime, kdyz answer == null (uzivatel klikl na cancel v dialogu) nebo kdyz zadal konec
+        //} while (answer != null && !answer.equals("konec"));
     }
 }
